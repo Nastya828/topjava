18c5d4895f0f432bd332467914030e20ec8c912f
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-10-14 14:27:10 +0300

    HW1 fixed 1.0

--- src/main/java/ru/javawebinar/topjava/repository/InMemoryMealsRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/InMemoryMealsRepository.java
@@ -13,24 +13,22 @@ public class InMemoryMealsRepository implements MealsRepository {
     private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private AtomicInteger idCounter = new AtomicInteger(0);
 
-    {
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000));
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500));
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
-
-    }
+//    {
+//        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
+//        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
+//        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
+//        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
+//        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000));
+//        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500));
+//        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
+//
+//    }
 
     @Override
     public Meal save(Meal meal) {
         if (meal.getId() == null) {
             meal.setId(idCounter.getAndIncrement());
-            System.out.println("idCounter " + idCounter);
         }
-        System.out.println(meal.getId());
         return repository.put(meal.getId(), meal);
     }
 
